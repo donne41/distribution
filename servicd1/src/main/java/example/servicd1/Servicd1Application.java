8bf8a6acbf2cf056e815f5de3dc486ec8bc8ca82
danne <xsz300@gmail.com>
2026-05-13 09:16:23 +0200

    added properties and main java class

---
+++ servicd1/src/main/java/example/servicd1/Servicd1Application.java
@@ -0,0 +1,13 @@
+package example.servicd1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Servicd1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Servicd1Application.class, args);
+    }
+
+}
